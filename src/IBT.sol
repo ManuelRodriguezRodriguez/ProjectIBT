b84c661df8348afbfbe220492442e7966a6c9221
ManuelRodriguezRodriguez <m.rodriguez10@udc.es>
2025-01-26 19:56:20 +0200

    Change to the frontend and contracts

--- src/IBT.sol
+++ src/IBT.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract IBT is ERC20, Ownable {
     constructor() 
         ERC20("IBT Token", "IBT") 
-        Ownable(msg.sender) // Pass the initial owner here
-    {
-        // Optionally mint some initial supply, etc.
-        // _mint(msg.sender, 1000 * 10**decimals());
-    }
+        Ownable(msg.sender)
 
+    {	
+          }
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
